418dfe4a31e852899c69fafbb25b204532f492b8
Audrey Li <me@audreyli.me>
2019-07-22 12:25:05 -0700

    Fix lint

--- packages/regl-worldview/src/Worldview.js
+++ packages/regl-worldview/src/Worldview.js
@@ -212,7 +212,6 @@ export class WorldviewBase extends React.Component<BaseProps, State> {
     worldviewContext
       .readHitmap(canvasX, canvasY)
       .then((objectId) => {
-        console.log("objectId: ", objectId);
         if (worldviewHandler) {
           handleWorldviewMouseInteraction(worldviewContext.getDrawPropByHitmapId(objectId), ray, e, worldviewHandler);
         }
